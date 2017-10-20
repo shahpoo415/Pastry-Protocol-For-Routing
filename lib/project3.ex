c8548ee62bacd3aa14007115db13da580554218b
shahpoo415 <shahpoo415@gmail.com>
2017-10-19 22:55:01 -0400

    Update project3.ex

--- lib/project3.ex
+++ lib/project3.ex
@@ -1,3 +1,26 @@
 defmodule Project3 do
- 
+  @moduledoc """
+  Documentation for Project3.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Project3.hello
+      :world
+
+  """
+  def hello do
+    Manager.start_link
+    {:ok, pid1} = Tracker.start_link
+    Enum.each 1..10, fn x ->
+      Manager.start_node(pid1,x)
+    end
+    Tracker.get_list(pid1)
+    
+  end
+
 end
+
