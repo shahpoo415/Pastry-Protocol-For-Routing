77c226bb81493e0c834f4a0acf28845838ed95a7
ayushigarg1992 <garg.bulbul@gmail.com>
2017-10-19 20:33:39 -0400

    list maintenance

--- lib/routing.ex
+++ lib/routing.ex
@@ -1,6 +1,18 @@
 defmodule Routing do
-    def prefix_matcher(len) do
+    def prefix_matcher(len,id,list,i,list2) do
+        node_slice = String.slice(id,0..len-1)
+        item = Enum.at(list,i)
+        item_slice = String.slice(item,0..len-1)
+        if(item_slice==node_slice) do
+            list2 = [item|list2]
+        #IO.inspect list2
+        end
+        if i+1<length list do
+        prefix_matcher(len,id,list,i+1,list2)
         
+        else
+            list3 = list2 
+        end
     end
-    
+
 end
